9d04e83461d7cc76df75e9736109de359392dc76
jrichardson0617 <john.richardson.0617@gmail.com>
2021-05-17 10:20:37 -0400

    Initial commit

---
+++ src/main/java/com/jrichardson/immunization/ImmunizationApplication.java
@@ -0,0 +1,13 @@
+package com.jrichardson.immunization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmunizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImmunizationApplication.class, args);
+    }
+
+}
